b636427cadfc62c4b46b8855522c2f8b7ca325fa
moshi016 <mdmoshi@yahoo.com>
2024-08-20 23:37:56 -0400

    update 2

--- MyFirstAzureWebApp/obj/Release/net8.0/MyFirstAzureWebApp.AssemblyInfo.cs
+++ MyFirstAzureWebApp/obj/Release/net8.0/MyFirstAzureWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c316a292db4107223d0bf32e03ac34286aeb1f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e902d7b6cdb455c623d4eb284d616989993b0923")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
